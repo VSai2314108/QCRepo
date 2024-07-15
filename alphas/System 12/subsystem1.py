38d6f89d665a0193a193978496c2daee74edbb2b
Your Name <mattdeweyx@gmail.com>
2024-07-15 03:58:52 +0100

    using risk_off_strategy and risk_on_strategy calls

--- alphas/System 12/subsystem1.py
+++ alphas/System 12/subsystem1.py
@@ -11,7 +11,6 @@ from utils.indicators.VolatilityQM import VolatilityQM
 class BullOrHedge(AlphaQM):
     def __init__(self, customAlgo: QCAlgorithm) -> None:
         symbols = ['TMF', 'SQQQ', 'UVXY', 'GLD', 'SPY', 'UUP', 'SPXL', 'SPXU']
-
         indicators = [
             (RelativeStrengthIndexQM, 10), (MaxDrawdownQM, 10),
             (CumulativeReturnQM, 5), (CumulativeReturnQM, 1),
@@ -19,185 +18,89 @@ class BullOrHedge(AlphaQM):
             (MovingAverageQM, 200), (VolatilityQM, 30)
         ]        
         AlphaQM.__init__(self, customAlgo, (14, 4, 10, -10), symbols, indicators, True)
-        
+    
     def calculate(self):
-        ### PRICE ###
         spy_price_0 = self.customAlgo.indicators["SPY"]["tempBar"].Close
-        spxl_price_0 = self.customAlgo.indicators["SPXL"]["tempBar"].Close
-        
-        ### INDICATORS ###
-        spy_price_200 = self.customAlgo.indicators["SPY"]["MovingAverageQM_200"].temp_value
-        spxu_rsi_10 = self.customAlgo.indicators["SPXU"]["RelativeStrengthIndexQM_10"].temp_value
-        spy_maxdrawdown_10 = self.customAlgo.indicators["SPY"]["MaxDrawdownQM_10"].temp_value
-        tmf_maxdrawdown_10 = self.customAlgo.indicators["TMF"]["MaxDrawdownQM_10"].temp_value
-        spy_ema_200 = self.customAlgo.indicators["SPY"]["ExponentialMovingAverageQM_200"].temp_value
-        spxl_rsi_10 = self.customAlgo.indicators["SPXL"]["RelativeStrengthIndexQM_10"].temp_value
-        spy_cumret_5 = self.customAlgo.indicators["SPY"]["CumulativeReturnQM_5"].temp_value
-        spxl_cumret_1 = self.customAlgo.indicators["SPXL"]["CumulativeReturnQM_1"].temp_value
-        spy_rsi_10 = self.customAlgo.indicators["SPY"]["RelativeStrengthIndexQM_10"].temp_value
-        uvxy_rsi_10 = self.customAlgo.indicators["UVXY"]["RelativeStrengthIndexQM_10"].temp_value
-        spxl_ma_20 = self.customAlgo.indicators["SPXL"]["MovingAverageQM_20"].temp_value
-        sqqq_rsi_10 = self.customAlgo.indicators["SQQQ"]["RelativeStrengthIndexQM_10"].temp_value
+        spy_ma_200 = self.customAlgo.indicators["SPY"]["MovingAverageQM_200"].temp_value
 
-        tickers_weights = []
-        if spy_price_0 > spy_price_200:
+        if spy_price_0 > spy_ma_200:
+            spxu_rsi_10 = self.customAlgo.indicators["SPXU"]["RelativeStrengthIndexQM_10"].temp_value
             if spxu_rsi_10 > 79.0:
-                tickers_weights.append(("UVXY", 1))
+                return self.allocate([("UVXY", 1)])
             else:
-                if spy_maxdrawdown_10 > 6.0 or tmf_maxdrawdown_10 > 7.0:
-                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                else:
-                    if spy_price_0 > spy_ema_200:
-                        if spxl_rsi_10 > 79.0:
-                            tickers_weights.append(("UVXY", 1))
-                        else:
-                            if spy_cumret_5 < -6.0:
-                                if spxl_cumret_1 > 5.0 or spxl_rsi_10 > 31.0:
-                                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                else:
-                                    tickers_weights.append(("SPXL", 1))
-                            else:
-                                if spy_rsi_10 < 31.0:
-                                    tickers_weights.append(("SPXL", 1))
-                                else:
-                                    tickers_weights.append(("SPY", 1))
-                    else:
-                        if spxl_rsi_10 < 31.0:
-                            tickers_weights.append(("SPXL", 1))
-                        else:
-                            if uvxy_rsi_10 > 74.0:
-                                if uvxy_rsi_10 > 84.0:
-                                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                else:
-                                    tickers_weights.append(("UVXY", 1))
-                            else:
-                                if spxl_price_0 > spxl_ma_20:
-                                    if sqqq_rsi_10 < 31.0:
-                                        tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                    else:
-                                        tickers_weights.append(("SPXL", 1))
-                                else:
-                                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
+                return self.risk_off_strategy()
         else:
+            spxu_rsi_10 = self.customAlgo.indicators["SPXU"]["RelativeStrengthIndexQM_10"].temp_value
             if spxu_rsi_10 < 31.0:
-                if spy_maxdrawdown_10 > 6.0 or tmf_maxdrawdown_10 > 7.0:
-                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                else:
-                    if spy_price_0 > spy_ema_200:
-                        if spxl_rsi_10 > 79.0:
-                            tickers_weights.append(("UVXY", 1))
-                        else:
-                            if spy_cumret_5 < -6.0:
-                                if spxl_cumret_1 > 5.0 or spxl_rsi_10 > 31.0:
-                                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                else:
-                                    tickers_weights.append(("SPXL", 1))
-                            else:
-                                if spy_rsi_10 < 31.0:
-                                    tickers_weights.append(("SPXL", 1))
-                                else:
-                                    tickers_weights.append(("SPY", 1))
-                    else:
-                        if spxl_rsi_10 < 31.0:
-                            tickers_weights.append(("SPXL", 1))
-                        else:
-                            if uvxy_rsi_10 > 74.0:
-                                if uvxy_rsi_10 > 84.0:
-                                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                else:
-                                    tickers_weights.append(("UVXY", 1))
-                            else:
-                                if spxl_price_0 > spxl_ma_20:
-                                    if sqqq_rsi_10 < 31.0:
-                                        tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                    else:
-                                        tickers_weights.append(("SPXL", 1))
-                                else:
-                                    tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
+                return self.risk_off_strategy()
             else:
+                uvxy_rsi_10 = self.customAlgo.indicators["UVXY"]["RelativeStrengthIndexQM_10"].temp_value
                 if uvxy_rsi_10 > 74.0:
                     if uvxy_rsi_10 > 84.0:
-                        if spy_maxdrawdown_10 > 6.0 or tmf_maxdrawdown_10 > 7.0:
-                            tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                        else:
-                            if spy_price_0 > spy_ema_200:
-                                if spxl_rsi_10 > 79.0:
-                                    tickers_weights.append(("UVXY", 1))
-                                else:
-                                    if spy_cumret_5 < -6.0:
-                                        if spxl_cumret_1 > 5.0 or spxl_rsi_10 > 31.0:
-                                            tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                        else:
-                                            tickers_weights.append(("SPXL", 1))
-                                    else:
-                                        if spy_rsi_10 < 31.0:
-                                            tickers_weights.append(("SPXL", 1))
-                                        else:
-                                            tickers_weights.append(("SPY", 1))
-                            else:
-                                if spxl_rsi_10 < 31.0:
-                                    tickers_weights.append(("SPXL", 1))
-                                else:
-                                    if uvxy_rsi_10 > 74.0:
-                                        if uvxy_rsi_10 > 84.0:
-                                            tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                        else:
-                                            tickers_weights.append(("UVXY", 1))
-                                    else:
-                                        if spxl_price_0 > spxl_ma_20:
-                                            if sqqq_rsi_10 < 31.0:
-                                                tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                            else:
-                                                tickers_weights.append(("SPXL", 1))
-                                        else:
-                                            tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
+                        return self.risk_off_strategy()
                     else:
-                        tickers_weights.append(("UVXY", 1))
+                        return self.allocate([("UVXY", 1)])
                 else:
-                    if spy_maxdrawdown_10 > 6.0 or tmf_maxdrawdown_10 > 7.0:
-                        tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                    else:
-                        if spy_price_0 > spy_ema_200:
-                            if spxl_rsi_10 > 79.0:
-                                tickers_weights.append(("UVXY", 1))
-                            else:
-                                if spy_cumret_5 < -6.0:
-                                    if spxl_cumret_1 > 5.0 or spxl_rsi_10 > 31.0:
-                                        tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                    else:
-                                        tickers_weights.append(("SPXL", 1))
-                                else:
-                                    if spy_rsi_10 < 31.0:
-                                        tickers_weights.append(("SPXL", 1))
-                                    else:
-                                        tickers_weights.append(("SPY", 1))
-                        else:
-                            if spxl_rsi_10 < 31.0:
-                                tickers_weights.append(("SPXL", 1))
-                            else:
-                                if uvxy_rsi_10 > 74.0:
-                                    if uvxy_rsi_10 > 84.0:
-                                        tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                    else:
-                                        tickers_weights.append(("UVXY", 1))
-                                else:
-                                    if spxl_price_0 > spxl_ma_20:
-                                        if sqqq_rsi_10 < 31.0:
-                                            tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
-                                        else:
-                                            tickers_weights.append(("SPXL", 1))
-                                    else:
-                                        tickers_weights = self.inverse_volatility_allocation(["GLD", "UUP"])
+                    return self.risk_off_strategy()
 
-        return self.allocate(tickers_weights)
+    def risk_off_strategy(self):
+        spy_drawdown_10 = self.customAlgo.indicators["SPY"]["MaxDrawdownQM_10"].temp_value
+        tmf_drawdown_10 = self.customAlgo.indicators["TMF"]["MaxDrawdownQM_10"].temp_value
+        
+        if spy_drawdown_10 > 6.0 or tmf_drawdown_10 > 7.0:
+            return self.allocate(self.inverse_volatility_allocation(["GLD", "UUP"]))
+        else:
+            return self.risk_on_strategy()
+
+    def risk_on_strategy(self):
+        spy_price_0 = self.customAlgo.indicators["SPY"]["tempBar"].Close
+        spy_ema_200 = self.customAlgo.indicators["SPY"]["ExponentialMovingAverageQM_200"].temp_value
+        if spy_price_0 > spy_ema_200:
+            spxl_rsi_10 = self.customAlgo.indicators["SPXL"]["RelativeStrengthIndexQM_10"].temp_value
+            if spxl_rsi_10 > 79.0:
+                return self.allocate([("UVXY", 1)])
+            else:
+                spy_return_5 = self.customAlgo.indicators["SPY"]["CumulativeReturnQM_5"].temp_value
+                if spy_return_5 < -6.0:
+                    spxl_return_1 = self.customAlgo.indicators["SPXL"]["CumulativeReturnQM_1"].temp_value
+                    if spxl_return_1 > 5.0 or spxl_rsi_10 > 31.0:
+                        return self.allocate(self.inverse_volatility_allocation(["GLD", "UUP"]))
+                    else:
+                        return self.allocate([("SPXL", 1)])
+                else:
+                    spy_rsi_10 = self.customAlgo.indicators["SPY"]["RelativeStrengthIndexQM_10"].temp_value
+                    if spy_rsi_10 < 31.0:
+                        return self.allocate([("SPXL", 1)])
+                    else:
+                        return self.allocate([("SPY", 1)])
+        else:
+            spxl_rsi_10 = self.customAlgo.indicators["SPXL"]["RelativeStrengthIndexQM_10"].temp_value
+            if spxl_rsi_10 < 31.0:
+                return self.allocate([("SPXL", 1)])
+            else:
+                uvxy_rsi_10 = self.customAlgo.indicators["UVXY"]["RelativeStrengthIndexQM_10"].temp_value
+                if uvxy_rsi_10 > 74.0:
+                    if uvxy_rsi_10 > 84.0:
+                        return self.allocate(self.inverse_volatility_allocation(["GLD", "UUP"]))
+                    else:
+                        return self.allocate([("UVXY", 1)])
+                else:
+                    spxl_price_0 = self.customAlgo.indicators["SPXL"]["tempBar"].Close
+                    spxl_ma_20 = self.customAlgo.indicators["SPXL"]["MovingAverageQM_20"].temp_value
+                    if spxl_price_0 > spxl_ma_20:
+                        sqqq_rsi_10 = self.customAlgo.indicators["SQQQ"]["RelativeStrengthIndexQM_10"].temp_value
+                        if sqqq_rsi_10 < 31.0:
+                            return self.allocate(self.inverse_volatility_allocation(["GLD", "UUP"]))
+                        else:
+                            return self.allocate([("SPXL", 1)])
+                    else:
+                        return self.allocate(self.inverse_volatility_allocation(["GLD", "UUP"]))
 
     def inverse_volatility_allocation(self, tickers):
         volatilities = [self.customAlgo.indicators[ticker]["VolatilityQM_30"].temp_value for ticker in tickers]
         inverse_volatilities = [1 / vol if vol != 0 else 0 for vol in volatilities]
         total_inverse_volatility = sum(inverse_volatilities)
-        
         if total_inverse_volatility == 0:
             return [(ticker, 1 / len(tickers)) for ticker in tickers]
-        
         weights = [inv_vol / total_inverse_volatility for inv_vol in inverse_volatilities]
         return list(zip(tickers, weights))
